44cce8aa3c88ce42e4a938fa7c8f51f4f6687e5b
MiaBorkoo <23391707@studentmail.ul.ie>
2023-12-07 21:20:16 +0000

    adding a map with modified main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,19 @@
 package org.example;
-
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
+import org.example.DataTypes.Location;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
+        
+        
+        CustomMap customMap = new CustomMap();
+        System.out.println("This is the inital map");
+        customMap.displayMap();
+
+        customMap.addVehicles();
+
+        // Display the map
+        customMap.displayMap();
 
     }
-    public static void loginSignup() {
-        Scanner scanner = new Scanner(System.in);
-
     }
-}
