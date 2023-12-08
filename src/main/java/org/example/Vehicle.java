6fbf5a802793179eea433e153da8ff7849a11492
MiaBorkoo <23391707@studentmail.ul.ie>
2023-12-08 12:08:36 +0000

    fixed it

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -1,14 +1,54 @@
 package org.example;
 
+import org.example.DataTypes.Location;
 import org.example.DataTypes.vehicleType;
 
 public class Vehicle {
-    String registrationNumber;
-    double driverRating;
-    vehicleType type;
-    public Vehicle(String registrationNumber, double driverRating, vehicleType type) {
+    private String registrationNumber;
+    private double driverRating;
+    private vehicleType type;
+    private boolean isAvailable;
+    private Location location;
+
+    public Vehicle(String registrationNumber, double driverRating, vehicleType type, Location location) {
         this.registrationNumber = registrationNumber;
         this.driverRating = driverRating;
         this.type = type;
+        this.location = location;
+    }
+//getter and setter methods for all the above variables
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+    public double getDriverRating() {
+       return driverRating;
+    }
+    public void setDriverRating(double driverRating) {
+        this.driverRating = driverRating;
+    }
+    public vehicleType getType() {
+        return type;
+    }
+    public void setType(vehicleType type) {
+        this.type = type;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+    public void printVehicleDetails() {
+        System.out.println("Registration Number: " + this.registrationNumber);
+        System.out.println("Driver Rating: " + this.driverRating);
+        System.out.println("Vehicle Type: " + this.type);
+        System.out.println("Is Available: " + this.isAvailable);
+    }
+    public Location getLocation() {
+        return location;
     }
 }
+
